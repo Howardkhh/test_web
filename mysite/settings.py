f64a61c1dc3d3f44d4aa60113db23a458427c22d
Howard Hou <howard.hou.fan@gmail.com>
2021-09-06 18:45:47 +0800

    added pillow for requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ebb@mb5l!aq4me3qcx7d1%f!sos1&=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["howardkhh-test.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
