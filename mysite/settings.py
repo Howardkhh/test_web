67d5cc52290e62cc803b6354175f1cb6bbc610f9
Howard Hou <howard.hou.fan@gmail.com>
2021-09-07 11:45:23 +0800

    added s3 service update

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,4 +148,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "howardkhhtestbucket"
-AWS_QUERYSTRING_AUTH = True
+AWS_QUERYSTRING_AUTH = False
