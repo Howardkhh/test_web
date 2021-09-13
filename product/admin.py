e600486774afe63a863af961e09a833233caa6a9
Howard Hou <howard.hou.fan@gmail.com>
2021-09-13 14:02:09 +0800

    changed admin look

--- product/admin.py
+++ product/admin.py
@@ -6,6 +6,9 @@ from .models import Product, Image
 
 class ImageAdmin(admin.StackedInline):
     model = Image
+    verbose_name = "照片"
+    verbose_name_plural = "商品照片"
+    extra = 0
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -13,7 +16,3 @@ class ProductAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Product
-
-# @admin.register(Image)
-# class ImageAdmin(admin.ModelAdmin):
-#     pass
--- product/models.py
+++ product/models.py
@@ -3,39 +3,52 @@ from django.urls import reverse
 from django.db.models.signals import pre_save
 from django.core.files import File
 from django.core.files.storage import FileSystemStorage
-from django.conf import settings
 from django.dispatch import receiver
 from PIL import Image as PIL_Image
-import os
 from io import BytesIO
-
-class OverwriteStorage(FileSystemStorage):
-    def get_available_name(self, name, *args, **kwargs):
-        if self.exists(name):
-            os.remove(os.path.join(settings.MEDIA_ROOT, name))
-        return name
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    detail = models.TextField()
-    price = models.IntegerField()
-    video = models.TextField(blank=True)
+    name = models.CharField("商品名稱", max_length=100)
+    detail = models.TextField("商品描述")
+    price = models.IntegerField("價格")
+    thumbnail = models.ImageField("商品圖像", upload_to='product/')
+    video = models.TextField("介紹影片", blank=True)
 
     def __str__(self):
         return "product " + self.name
 
-    def get_default_img(self):
-        return Product.objects.get(id=self.id).product_image.filter(default=True).first()
-
     def get_absolute_url(self):
         return reverse("product:product detail", kwargs={"slug": self.name})
 
+    class Meta:
+        
+        verbose_name = "商品"
+        verbose_name_plural = "商品"
+
 class Image(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField("照片名稱", max_length=100)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
-    image = models.ImageField(upload_to='product/')
-    default = models.BooleanField(default=False)
+    image = models.ImageField("照片檔案", upload_to='product/')
+
+    
+    def __str__(self) -> str:
+        return self.name
+
+@receiver(pre_save, sender=Product)
+def crop_img(sender, instance, *args, **kwargs):
+    if instance.thumbnail:
+        img = PIL_Image.open(instance.thumbnail)
+        w, h = img.width, img.height
+        ratio = 1
+        if w/h > ratio:
+            img = img.crop((int((w-h*ratio)/2), 0, int((w-h*ratio)/2)+int(h*ratio), h))
+        elif w/h < ratio:
+            img = img.crop((0, int((h-w/ratio)/2), w, int((h-w/ratio)/2)+int(w/ratio)))
+        img = img.resize((480, 480))
+        img_io = BytesIO()
+        img.save(img_io, "JPEG", quality=50)
+        instance.thumbnail = File(img_io, name=f'{instance.name}/thumbnail/{instance.name}_thumbnail.jpg')
 
 @receiver(pre_save, sender=Image)
 def crop_img(sender, instance, *args, **kwargs):
@@ -50,6 +63,6 @@ def crop_img(sender, instance, *args, **kwargs):
         if img.width > 1920:
             img = img.resize((1920, 1440))
         img_io = BytesIO()
-        img.save(img_io, "JPEG", quality=60)
-        instance.image = File(img_io, name=instance.product.name+'/'+instance.name+'.jpg')
+        img.save(img_io, "JPEG", quality=80)
+        instance.image = File(img_io, name=f'{instance.product.name}/product_images/{instance.name}.jpg')
 
